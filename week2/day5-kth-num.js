00c7b0ed204e52529268e68cb6645f4e53b174a4
hlezg08 <hyelin132@gmail.com>
2022-05-23 09:08:16 +0900

    7th commit : refactoring

--- week2/day5-kth-num.js
+++ week2/day5-kth-num.js
@@ -7,6 +7,12 @@ function solution(array, commands) {
 
     //Array.sort()의 경우 숫자가 배열로 들어와도 숫자 크기 순이 아니라 유니코드 순서로 정렬
     answer.push(array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]);
+
+    //Array.map 이용해서 풀기
+    // const answer = commands.map((num) => {
+    //   const result = array.slice(num[0] - 1, num[1]).sort((a, b) => a - b);
+    //   return result[num[2] - 1];
+    // });
   }
   return answer;
 }
--- week2/day5-string-descending.js
+++ week2/day5-string-descending.js
@@ -3,5 +3,9 @@ function solution(s) {
   s = s.split("");
   answer = s.sort().reverse().join("");
 
+  //다른 방법
+  // s=s.split("");
+  // s.sort((a,b)=>a<b?1:-1); //내림차순 정렬
+  // answer=s.join("");
   return answer;
 }
