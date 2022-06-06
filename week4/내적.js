665a7bd5c97381ef6142a19d8d550c7ef38d9cdf
hlezg08 <hyelin132@gmail.com>
2022-06-06 18:13:18 +0900

    두 정수 사이의 합 풀이, week4 리팩토링

--- week4/내적.js
+++ week4/내적.js
@@ -4,4 +4,9 @@ function solution(a, b) {
     answer += a[i] * b[i];
   }
   return answer;
+
+  //reduce 메소드 사용하는 법
+  //   return a.reduce((acc,cur,i)=>{
+  //     return acc+(cur*b[i])
+  // },0)
 }
--- week4/행렬의 덧셈.js
+++ week4/행렬의 덧셈.js
@@ -1,5 +1,5 @@
 function solution(arr1, arr2) {
-  var answer = [];
+  const answer = [];
   for (let i = 0; i < arr1.length; i++) {
     let tmp = [];
     for (let j = 0; j < arr2[i].length; j++) {
@@ -7,5 +7,23 @@ function solution(arr1, arr2) {
     }
     answer.push(tmp);
   }
+
+  // 또 다른 풀이 : 2차원 배열 동적할당하는 방법
+  // const answer = [[]];
+  // for (let i = 0; i < arr1.length; i++) {
+  //   for (let j = 0; j < arr1[i].length; j++) {
+  //     const sum = arr1[i][j] + arr2[i][j];
+  //     if (answer[i] === undefined) answer[i] = [];
+  //     answer[i][j] = sum;
+  //   }
+  // }
+
+  // 또 다른 풀이 : map 이용
+  // return arr1.map((num1, i) => {
+  //   return num1.map((num2, j) => {
+  //     return num2 + arr2[i][j];
+  //   });
+  // });
+
   return answer;
 }
