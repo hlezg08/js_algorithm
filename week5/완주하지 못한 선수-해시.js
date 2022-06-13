7ec0f67ad60c05511ceb713c68907aebe6d8a567
hlezg08 <hyelin132@gmail.com>
2022-06-13 18:31:31 +0900

    완주하지 못한 선수 해시 복습

--- week5/완주하지 못한 선수-해시.js
+++ week5/완주하지 못한 선수-해시.js
@@ -1,6 +1,5 @@
 function solution(participant, completion) {
   let hash = {};
-  let answer = "";
 
   // object 타입으로 hash(해시테이블) 초기화
   for (let el of participant) hash[el] = 0;
@@ -13,8 +12,25 @@ function solution(participant, completion) {
 
   // value가 0이 아닌 key값을 반환
   for (let key in hash) {
-    if (hash[key] !== 0) answer = key;
+    if (hash[key] !== 0) return key;
   }
 
-  return answer;
+  // const answer = {};
+
+  // // 1. 참가한 선수 이름과 수를 정리
+  // for (let i = 0; i < participant.length; i++) {
+  //   answer[participant[i]] === undefined
+  //     ? (answer[participant[i]] = 1)
+  //     : answer[participant[i]]++;
+  // }
+
+  // // 2. 참가자 명단에서 완주한 사람의 이름을 제거
+  // for (let i = 0; i < completion.length; i++) {
+  //   answer[completion[i]]--;
+  // }
+
+  // // 3. 완주하지 못한 선수 찾기
+  // for (let key in answer) {
+  //   if (answer[key] !== 0) return key;
+  // }
 }
