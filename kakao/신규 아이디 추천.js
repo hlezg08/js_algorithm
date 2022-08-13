62966324a5105280c89db6871dd9ecae9caeb8fb
hlezg08 <hyelin132@gmail.com>
2022-08-13 23:53:40 +0900

    신규 아이디 추천 solved

--- kakao/신규 아이디 추천.js
+++ kakao/신규 아이디 추천.js
@@ -1,7 +1,7 @@
-function solution(new_id) {
-  let cnt = 0;
-  let sub = "";
+// 실패 : 3,8,15,22,23,25,26번 케이스
+// 3번 케이스 수정 후 성공
 
+function solution(new_id) {
   // 1,2단계
   let id = new_id.toLowerCase();
   for (let letter of id) {
@@ -12,30 +12,27 @@ function solution(new_id) {
     else id = id.replace(letter, "");
   }
   console.log("2단계 " + id);
-  // 3단계
-  for (let letter of id) {
-    if (letter === ".") {
-      cnt++;
-      sub += letter;
-      id = id.replace(sub, ".");
-    } else {
-      if (cnt > 1) id = id.replace(sub, ".");
-      cnt = 0;
-      sub = "";
-    }
+
+  // 3단계 -> 실패 원인
+  while (id.includes("..")) {
+    id = id.replace("..", ".");
   }
   console.log("3단계 " + id);
+
   // 4단계
   if (id[0] === ".") id = id.substring(1, id.length);
   if (id[id.length - 1] === ".") id = id.substring(0, id.length - 1);
   console.log("4단계 " + id);
+
   // 5단계
   if (id.length === 0) id = "a";
   console.log("5단계 " + id);
+
   // 6단계
   if (id.length >= 16) id = id.substring(0, 15);
   if (id[id.length - 1] === ".") id = id.substring(0, id.length - 1);
   console.log("6단계 " + id);
+
   // 7단계
   if (id.length <= 2) {
     let s = id[id.length - 1];
