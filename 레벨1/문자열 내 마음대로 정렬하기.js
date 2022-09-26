65ceed3cdf401270240db47ab8947333c206df15
hlezg08 <hyelin132@gmail.com>
2022-09-26 15:26:14 +0900

    문자열 localeCompare 사용 문제, 부족한 금액 계산 solved

---
+++ 레벨1/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,12 @@
+function solution(strings, n) {
+  return strings.sort((a, b) => {
+    if (a[n] > b[n]) return 1;
+    else if (a[n] < b[n]) return -1;
+    else return a.localeCompare(b);
+  });
+
+  //   간단하게 줄이면 (다른 풀이 참고)
+  //   return strings.sort((a, b) =>
+  //     a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]),
+  //   );
+}
