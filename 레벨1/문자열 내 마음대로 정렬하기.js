373efcc249dea75443288e803e9058172a4823cd
hlezg08 <hyelin132@gmail.com>
2022-09-30 23:50:25 +0900

    문자열 내 마음대로 정렬하기 localCompare 사용 없이 풀기

--- 레벨1/문자열 내 마음대로 정렬하기.js
+++ 레벨1/문자열 내 마음대로 정렬하기.js
@@ -9,4 +9,11 @@ function solution(strings, n) {
   //   return strings.sort((a, b) =>
   //     a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]),
   //   );
+
+  // localeCompare 없이 풀기
+  // return strings.sort((a, b) => {
+  //   if (a[n] > b[n]) return 1;
+  //   else if (a[n] < b[n]) return -1;
+  //   else return a < b ? -1 : 1;
+  // });
 }
