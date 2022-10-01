7fdf47c9160e586bc8874b1f8e5c2ed2df615254
hlezg08 <hyelin132@gmail.com>
2022-10-01 15:38:51 +0900

    최소직사각형 failed
    정확도 65%

---
+++ search/완전탐색/최소직사각형.js
@@ -0,0 +1,20 @@
+function rotate(arr) {
+  [arr[0], arr[1]] = [arr[1], arr[0]];
+  return arr;
+}
+
+function solution(sizes) {
+  let maxWidth = sizes[0][0];
+  let maxHeight = sizes[0][1];
+
+  for (let i = 0; i < sizes.length; i++) {
+    if (sizes[i][0] < sizes[i][1]) rotate(sizes[i]);
+  }
+
+  for (let i = 0; i < sizes.length; i++) {
+    if (maxWidth < sizes[i][0]) maxWidth = sizes[i][0];
+    if (maxHeight < sizes[i][1]) maxHeight = sizes[i][1];
+  }
+
+  return maxWidth * maxHeight;
+}
