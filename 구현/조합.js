010fcba9a73634e5b890cba31c79ce98ebbce1e0
hlezg08 <hyelin132@gmail.com>
2022-10-01 15:51:53 +0900

    조합 구현

---
+++ 구현/조합.js
@@ -0,0 +1,13 @@
+let arr = [0, 1, 2, 3];
+function getCombination(arr, num) {
+  let result = [];
+  if (num === 1) return arr.map(el => [el]);
+  arr.forEach((el, idx, origin) => {
+    let rest = origin.slice(idx + 1);
+    let combination = getCombination(rest, num - 1);
+    let newArr = combination.map(v => [el, ...v]);
+    result.push(...newArr);
+  });
+  return result;
+}
+getCombination(arr, 2);
