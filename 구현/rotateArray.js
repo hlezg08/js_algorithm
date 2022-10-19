8b2620daa5c133e1938194d9845fe2cc618d3e31
hlezg08 <hyelin132@gmail.com>
2022-10-19 23:22:01 +0900

    LeetCode Rotate Array 해결
    주어진 힌트 사용: 전체 reverse->(0~k-1) reverse->(k~끝) reverse
    k가 배열 크기보다 커지는 경우 나머지 연산으로 대입->아직 이해 못함

--- 구현/rotateArray.js
+++ 구현/rotateArray.js
@@ -4,10 +4,20 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
-  let i = 0;
-  while (i < k) {
-    let num = nums.splice(nums.length - 1, 1);
-    nums.unshift(num);
-    i++;
-  }
+  if (nums.length <= 1) return nums;
+  if (k >= nums.length) k %= nums.length;
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
 };
+
+function reverse(arr, start, end) {
+  if (start < 0 || end > arr.length || start >= end) return;
+  let startIdx = start;
+  let endIdx = end;
+  while (startIdx < endIdx) {
+    [arr[startIdx], arr[endIdx]] = [arr[endIdx], arr[startIdx]];
+    startIdx += 1;
+    endIdx -= 1;
+  }
+}
