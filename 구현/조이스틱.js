0a3fe5cc86af31e29d400dd857e80635d3c4345a
hlezg08 <hyelin132@gmail.com>
2022-10-22 16:55:59 +0900

    조이스틱 failed
    일부 케이스만 통과, 커서 이동 최솟값 못 구함

--- 구현/조이스틱.js
+++ 구현/조이스틱.js
@@ -1,13 +1,42 @@
+// 12,13,15,18~22,24,25,27 실패
 function solution(name) {
   const aCode = 'A'.charCodeAt();
   const zCode = 'Z'.charCodeAt();
-  let count = 0;
+  const arr = [0];
+  let answer = 0;
 
-  for (let letter of name) {
-    const letterCode = letter.charCodeAt();
-    count += Math.min(letterCode - aCode, zCode - letterCode + 1);
-    count += 1;
+  // arr 부분 이해 못함
+  for (let i = 0; i < name.length; i++) {
+    const letterCode = name[i].charCodeAt();
+
+    // 문자가 A인 경우, 아닌 경우
+    if (name[i] === 'A') {
+      if (i === 0) arr.push(findConsecutiveA(name, i) - 1);
+      else if (name[i - 1] !== 'A')
+        arr.push(findConsecutiveA(name, i) - (i - 1));
+      answer++;
+    } else answer += Math.min(letterCode - aCode, zCode - letterCode + 1) + 1;
   }
 
-  return count - 1;
+  return answer - Math.max(...arr) - 1;
 }
+
+// idx 시점으로부터 A가 연속으로 나오는 횟수 구하기
+const findConsecutiveA = (name, idx) => {
+  let cnt = 0;
+  let cur = -1;
+
+  for (let i = idx; i < name.length; i++) {
+    if (name[i] === 'A') {
+      cur = i;
+      break;
+    }
+  }
+
+  for (let i = cur; i < name.length; i++) {
+    if (name[i] === 'A') cnt++;
+    else break;
+  }
+
+  return cnt;
+};
