cec1a79fb999137ea6b6d9bc8788869bf3879585
hlezg08 <hyelin132@gmail.com>
2022-10-26 23:40:54 +0900

    게임 맵 최단거리 solved

--- search/게임맵 최단거리.js
+++ search/게임맵 최단거리.js
@@ -1,27 +1,23 @@
 const dx = [0, 0, 1, -1];
 const dy = [1, -1, 0, 0];
 
-function bfs(array, x, y) {
-  const n = array.length;
-  const m = array[0].length;
+function solution(maps) {
+  const n = maps.length;
+  const m = maps[0].length;
   let queue = [];
-  queue.push([x, y]);
+  queue.push([0, 0]);
   while (queue.length > 0) {
     let [x, y] = queue.shift();
     for (let i = 0; i < 4; i++) {
       let nx = x + dx[i];
       let ny = y + dy[i];
       if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-      if (array[nx][ny] === 0) continue;
-      if (array[nx][ny] === 1) {
-        array[nx][ny] = array[x][y] + 1;
+      if (maps[nx][ny] === 0) continue;
+      if (maps[nx][ny] === 1) {
+        maps[nx][ny] = maps[x][y] + 1;
         queue.push([nx, ny]);
       }
     }
   }
-  return array[n - 1][m - 1] === 1 ? -1 : array[n - 1][m - 1];
-}
-
-function solution(maps) {
-  return bfs(maps, 0, 0);
+  return maps[n - 1][m - 1] === 1 ? -1 : maps[n - 1][m - 1];
 }
